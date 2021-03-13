4e459ccd170ff9da8844b7f7d68501046d77e1be
zanjie1999 <zanjie1999@yeah.net>
2021-03-14 01:55:59 +0800

    sdraw

--- libraries/SD/src/sd_diskio.h
+++ libraries/SD/src/sd_diskio.h
@@ -27,5 +27,7 @@ uint8_t sdcard_unmount(uint8_t pdrv);
 sdcard_type_t sdcard_type(uint8_t pdrv);
 uint32_t sdcard_num_sectors(uint8_t pdrv);
 uint32_t sdcard_sector_size(uint8_t pdrv);
+bool sd_read_raw(uint8_t pdrv, uint8_t* buffer, uint32_t sector);
+bool sd_write_raw(uint8_t pdrv, uint8_t* buffer, uint32_t sector);
 
 #endif /* _SD_DISKIO_H_ */
